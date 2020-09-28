bca55808701627fae326aea7338f998684b93c79
Valentin Vieriu <valentin.vieriu@sap.com>
2020-09-28 18:22:30 +0200

    added cache for graphql

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,6 @@
 <template>
   <nav class="bg-white px-8 pt-2 shadow-md">
-    <div class="-mb-px flex justify-center">
-      <router-link
-        :to="{ name: 'Home'}"
-        exact-active-class="border-teal-600 text-teal-600"
-        class="no-underline text-grey-600 border-b-2 uppercase tracking-wide font-bold text-xs py-3 mr-8"
-      >Home</router-link>
-      <router-link
-        :to="{ name: 'About'}"
-        exact-active-class="border-teal-600 text-teal-600"
-        class="no-underline text-grey-600 border-b-2 uppercase tracking-wide font-bold text-xs py-3 mr-8"
-      >About</router-link>
-      <router-link
-        v-if="isAuthenticated"
-        :to="{ name: 'Logout'}"
-        exact-active-class="border-teal-600 text-teal-600"
-        class="no-underline text-grey-600 border-b-2 uppercase tracking-wide font-bold text-xs py-3"
-      >Logout</router-link>
-    </div>
+    <navigation></navigation>
   </nav>
   <div class="bg-white px-8 pt-2 shadow-md mt-10">
     <router-view></router-view>
@@ -25,16 +8,12 @@
 </template>
 
 <script>
-import { useStore } from 'vuex';
-import { computed } from 'vue';
+import navigation from "./components/Navigation.vue";
 export default {
+  components: {
+    navigation,
+  },
   name: "App",
-  setup() {
-    const store = useStore();
-    const isAuthenticated = computed(() => store.getters.isAuthenticated);
-    return {
-      isAuthenticated
-    }
-  },  
+  setup() {},
 };
 </script>
