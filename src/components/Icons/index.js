4b16f50a746ffeee3121799c58984754e81c9dc8
Fabian Klöckner <fabian.kloeckner@brickmakers.de>
2020-10-05 11:52:50 +0200

    #5 Extract icons into components
    commit using correct account

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -1,9 +1,15 @@
 import LogoIcon from "./LogoIcon.vue";
 import NotificationIcon from "./NotificationIcon.vue";
 import PieIcon from "./PieIcon.vue";
+import TeamIcon from "./TeamIcon.vue";
+import ShoppingCardIcon from "./ShoppingCardIcon.vue";
+import ShoppingBagIcon from "./ShoppingBagIcon.vue";
 
 export {
   LogoIcon,
   NotificationIcon,
   PieIcon,
+  TeamIcon,
+  ShoppingCardIcon,
+  ShoppingBagIcon,
 };
