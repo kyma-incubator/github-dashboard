fa87bd9e1428a04cb81dcb7691861333bfa49258
Nur Intan Alatas <intannnalatas@gmail.com>
2020-10-05 14:51:13 +0700

    change Icon folder name, add export default

---
+++ src/components/Icons/index.js
@@ -0,0 +1,9 @@
+import LogoIcon from "./LogoIcon.vue";
+import NotificationIcon from "./NotificationIcon.vue";
+import PieIcon from "./PieIcon.vue";
+
+export {
+  LogoIcon,
+  NotificationIcon,
+  PieIcon,
+};
