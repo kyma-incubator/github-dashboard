f1e5056294af2a400101ab376269f299f906b63a
Valentin Vieriu <valentin.vieriu@gmail.com>
2020-10-11 18:57:21 +0200

    added search to teams

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -6,32 +6,6 @@
       </h2>
 
       <div class="mt-3 flex flex-col sm:flex-row">
-        <div class="flex">
-          <div class="relative">
-            <select
-              class="appearance-none h-full rounded border-t sm:rounded-r-none sm:border-r-0 border-r border-b border-l block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500"
-            >
-              <option>All</option>
-              <option>Active</option>
-              <option>Inactive</option>
-            </select>
-
-            <div
-              class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
-            >
-              <svg
-                class="fill-current h-4 w-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
-                />
-              </svg>
-            </div>
-          </div>
-        </div>
-
         <div class="block relative mt-2 sm:mt-0">
           <span class="absolute inset-y-0 left-0 flex items-center pl-2">
             <svg viewBox="0 0 24 24" class="h-4 w-4 fill-current text-gray-500">
@@ -43,11 +17,12 @@
 
           <input
             placeholder="Search"
-            class="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"
+            v-model="searchTerm"
+            class="appearance-none rounded border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"
           />
         </div>
       </div>
-
+            {{selectedItem}} - {{searchTerm}}
       <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
         <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
           <table class="min-w-full leading-normal">
@@ -81,7 +56,7 @@
               </tr>
             </thead>
             <tbody>
-              <tr v-for="(u, index) in allMembers" :key="index">
+              <tr v-for="(u, index) in filteredMembers" :key="index">
                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                   <div class="flex items-center">
                     <div class="flex-shrink-0 w-10 h-10">
@@ -145,7 +120,7 @@
 
 <script>
 import { useStore } from "vuex";
-import { computed } from "vue";
+import { computed, ref, watch } from "vue";
 export default {
   name: "Home",
   props: {
@@ -153,6 +128,8 @@ export default {
   },
   setup() {
     const store = useStore();
+    let selectedItem = ref('All');
+    let searchTerm = ref('');
     const allMembers = computed(() => {
       const initial = store.getters.allMembers;
       const sorted = Object.keys(initial)
@@ -166,18 +143,37 @@ export default {
         }, {});
       return sorted;
     });
-    if (
-      !(
-        allMembers &&
+    let hasMembers = computed(() => {
+      if (allMembers &&
         allMembers.value &&
-        Object.keys(allMembers.value).length > 0
-      )
-    ) {
+        Object.keys(allMembers.value).length > 0) {
+        return true;
+      } else {
+        return false
+      }
+    })
+    let filteredMembers = computed(()=>{
+      let filteredResult = {};
+      if (hasMembers.value) {
+        Object.keys(allMembers.value).map( userLogin => {
+          const textVersion = JSON.stringify(allMembers.value[userLogin]).toLowerCase()
+          if (textVersion.includes(searchTerm.value.toLowerCase().trim())) {
+            filteredResult[userLogin] = allMembers.value[userLogin];
+          }
+        })        
+      } 
+      console.log(filteredResult)
+      return filteredResult;
+    });
+    if (!hasMembers.value) {
       console.log("getInitData", allMembers && allMembers.value);
       store.dispatch("getInitData");
     }
     return {
       allMembers,
+      filteredMembers,
+      selectedItem,
+      searchTerm
     };
   },
 };
